e93258a58cf542b1c949ae969fa26c6dcd1dcbf0
Henning Vogt <henning@vogt.family>
2023-03-14 22:29:38 +0100

    Add settings to section

--- section.go
+++ section.go
@@ -1,8 +1,9 @@
 package main
 
 type Section struct {
-	Type    string            `yaml:"type" json:"type"`
-	Content map[string]string `yaml:"content" json:"content"`
+	Type     string                 `yaml:"type" json:"type"`
+	Content  map[string]interface{} `yaml:"content" json:"content"`
+	Settings map[string]interface{} `yaml:"settings" json:"settings"`
 }
 
 func (s *Section) Render() error {
